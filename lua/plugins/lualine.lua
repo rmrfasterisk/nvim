13bd1b3b4156c2473759fb5f2f4092718ec59553
Sai Praneeth Reddy <spreddy617@gmail.com>
2022-03-05 17:27:35 +0530

    update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,18 +7,23 @@ require("lualine").setup {
   options = {
     icons_enabled = true,
     theme = "onedark",
-    component_separators = { left = "", right = ""},
-    section_separators = { left = "", right = ""},
+    component_separators = { left = "", right = ""},
+    section_separators = { left = "", right = ""},
     disabled_filetypes = {},
     always_divide_middle = true,
   },
   sections = {
-    lualine_a = {"mode"},
-    lualine_b = {"branch", "diff", "diagnostics"},
-    lualine_c = {"filename"},
-    lualine_x = {"encoding", "fileformat", "filetype"},
-    lualine_y = {"progress"},
-    lualine_z = {"location"}
+    lualine_a = {
+      { "mode", separator = { left = "" }, right_padding = 2 },
+    },
+    lualine_b = {"filename"},
+    lualine_c = {},
+    lualine_d = {},
+    lualine_x = { "diagnostics", "diff", "branch" },
+    lualine_y = {"filetype"},
+    lualine_z = {
+        { "location", separator = { right = "" }, left_padding = 2 },
+    }
   },
   inactive_sections = {
     lualine_a = {},
